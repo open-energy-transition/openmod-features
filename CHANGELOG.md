5387176580533b183c83222037621ce94effea83
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2025-10-16 17:15:24 +0100

    Update feature list following internal review

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -27,6 +27,22 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+### Removed
+
+- Removed "a.k.a." in feature descriptions in preparation for a separate `also_known_as` option
+
+### Changed
+
+- Renamed `ldc` -> `load_duration_curve`, `fluid/gas_distribution` -> `fluid_or_gas_distribution`, `fcf` -> `future_cost_function`.
+- Simplified `asset__operating_constraints` by grouping features that span the time dimension (ramping, run up, etc.) into a `inter_temporal` feature.
+- generalised `asset__operating_constraints.load_rate` into a `asset__operating_constraints.operating` feature for unit-commitment constraints.
+
+### Added
+
+- Added `asset__operating_constraints.inter_spatial` to capture operating constraints that span spatial regions.
+- Added `transmission__opf.transport` to clearly differentiate energy transport flows from DC power flow.
+- Added `postprocessing.aggregation` for methods to aggregate model components into broader classifications for dissemination.
+
 ## 0.1.0 (2025-10-16)
 
 Initial release.
--- schema/schema_generator.py
+++ schema/schema_generator.py
@@ -1,47 +1,18 @@
 """Tool feature schema generator."""
 
-from collections.abc import Hashable
 from pathlib import Path
-from typing import Annotated, Any, Literal, TypeVar
+from typing import Annotated, Any, Literal
 
 import click
 import pydantic
 import yaml
-from annotated_types import Len
-from pydantic import AfterValidator, Field, create_model
-from pydantic_core import PydanticCustomError, Url
+from pydantic import Field, create_model
+from pydantic_core import Url
 
 HttpsUrl = Annotated[
     Url, pydantic.UrlConstraints(max_length=2083, allowed_schemes=["https"])
 ]
 """A URL that must use the `https` scheme."""
-
-# ===
-# Modified from https://github.com/calliope-project/calliope/blob/9127604d7840d51486a66d12882b5e58d5aa4793/src/calliope/schemas/general.py
-T = TypeVar("T", bound=Hashable | list)
-
-
-def _validate_unique_list(v: list) -> list:
-    try:
-        unique = set(v)
-    except TypeError:
-        unique = set([tuple(i) for i in v])
-    if len(v) != len(unique):
-        raise PydanticCustomError("unique_list", "List must be unique")
-    return v
-
-
-UniqueList = Annotated[
-    list[T],
-    AfterValidator(_validate_unique_list),
-    Field(json_schema_extra={"uniqueItems": True}),
-]
-"""A list with no repeated values."""
-NonEmptyList = Annotated[list[T], Len(min_length=1)]
-"""A list with at least one value in it."""
-NonEmptyUniqueList = Annotated[UniqueList[T], Len(min_length=1)]
-"""A list with at least one value in it and no repeated values."""
-# ==
 
 FEATURES = yaml.safe_load((Path(__file__).parent / "features.yaml").read_text())
 
@@ -94,13 +65,14 @@ def create_feature_model() -> type[pydantic.BaseModel]:
 def cli():
     """Create a schema YAML file from the current state of the schema model."""
     feature_model_schema = create_feature_model()
-    (Path(__file__).parent / "schema.yaml").write_text(
-        yaml.safe_dump(feature_model_schema.model_json_schema())
-    )
+
+    cwd = Path(__file__).parent
+    schema_path = cwd / "schema.yaml"
+    schema_path.write_text(yaml.safe_dump(feature_model_schema.model_json_schema()))
 
     tool_feature_dict = feature_model_schema().model_dump(mode="json")
 
-    tool_feature_path = Path("template/features.yaml.jinja")
+    tool_feature_path = cwd / ".." / "template" / "features.yaml.jinja"
     header = "# yaml-language-server: $schema=https://raw.githubusercontent.com/open-energy-transition/openmod-features/{{ _copier_answers._commit }}/schema/schema.yaml"
     tool_feature_path.write_text(f"{header}\n{yaml.safe_dump(tool_feature_dict)}")
 
