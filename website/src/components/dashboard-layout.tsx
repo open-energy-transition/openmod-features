88dd9f6a05204446ce412e39ae85281b6e154c3b
mikoding <mikoding9@gmail.com>
2026-09-18 15:48:33 +0700

    Keep coverage rules visible while scrolling

--- website/src/components/dashboard-layout.tsx
+++ website/src/components/dashboard-layout.tsx
@@ -137,9 +137,11 @@ export function DashboardLayout() {
           onThemeChange={setTheme}
         />
         <div className="mx-auto grid max-w-[1800px] gap-5 px-4 py-6 sm:px-6 lg:px-8">
-          <div className="flex min-w-0 flex-col gap-3 lg:flex-row lg:items-center lg:justify-between">
-            <DashboardNav customUseCase={customUseCase} />
-            <CoverageControls options={coverageOptions} onChange={setCoverageOptions} />
+          <div className="atlas-sticky-toolbar sticky top-0 z-40 -mx-4 px-4 py-2 sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+            <div className="flex min-w-0 flex-col gap-3 lg:flex-row lg:items-center lg:justify-between">
+              <DashboardNav customUseCase={customUseCase} />
+              <CoverageControls options={coverageOptions} onChange={setCoverageOptions} />
+            </div>
           </div>
           {location.pathname === '/' ? <HomeIntro data={dashboardData} /> : null}
           <DashboardContext.Provider
